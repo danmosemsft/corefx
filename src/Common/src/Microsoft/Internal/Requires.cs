d42b59b5f7156c27cbce148bc9429bc954eeb791
danmosemsft <danmose@microsoft.com>
2017-09-20 23:08:43 -0700

    Port MEF1 sources

--- src/Common/src/Microsoft/Internal/Requires.cs
+++ src/Common/src/Microsoft/Internal/Requires.cs
@@ -38,5 +38,94 @@ namespace Microsoft.Internal
             }
             Contract.EndContractBlock();
         }
+
+        [DebuggerStepThrough]
+        [ContractArgumentValidator]
+        public static void NotNullOrNullElements<T>(IEnumerable<T> values, string parameterName)
+            where T : class
+        {
+            NotNull(values, parameterName);
+            NotNullElements(values, parameterName);
+            Contract.EndContractBlock();
+        }
+
+        [DebuggerStepThrough]
+        [ContractArgumentValidator]
+        public static void NullOrNotNullElements<TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> values, string parameterName)
+            where TKey : class
+            where TValue : class
+        {
+            NotNullElements(values, parameterName);
+            Contract.EndContractBlock();
+        }
+
+        [DebuggerStepThrough]
+        [ContractArgumentValidator]
+        public static void NullOrNotNullElements<T>(IEnumerable<T> values, string parameterName)
+            where T : class
+        {
+            NotNullElements(values, parameterName);
+            Contract.EndContractBlock();
+        }
+
+        [DebuggerStepThrough]
+        [ContractArgumentValidator]
+        private static void NotNullElements<T>(IEnumerable<T> values, string parameterName)
+            where T : class
+        {
+            if (values != null && !Contract.ForAll(values, (value) => value != null))
+            {
+                throw ExceptionBuilder.CreateContainsNullElement(parameterName);
+            }
+            Contract.EndContractBlock();
+        }
+
+        [DebuggerStepThrough]
+        public static void NullOrNotNullElements<T>(T[] values, string parameterName)
+            where T : class
+        {
+            NotNullElements(values, parameterName);
+        }
+
+        [DebuggerStepThrough]
+        private static void NotNullElements<T>(T[] values, string parameterName)
+            where T : class
+        {
+            if (values != null)
+            {
+                foreach (var value in values)
+                {
+                    if (value == null)
+                    {
+                        throw ExceptionBuilder.CreateContainsNullElement(parameterName);
+                    }
+                }
+            }
+        }
+
+        [DebuggerStepThrough]
+        [ContractArgumentValidator]
+        private static void NotNullElements<TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> values, string parameterName)
+            where TKey : class
+            where TValue : class
+        {
+            if (values != null && !Contract.ForAll(values, (keyValue) => keyValue.Key != null && keyValue.Value != null))
+            {
+                throw ExceptionBuilder.CreateContainsNullElement(parameterName);
+            }
+            Contract.EndContractBlock();
+        }
+
+        [DebuggerStepThrough]
+        [ContractArgumentValidator]
+        public static void IsInMembertypeSet(MemberTypes value, string parameterName, MemberTypes enumFlagSet)
+        {
+            if ((value & enumFlagSet) != value || // Ensure the member is in the set
+                (value & (value - 1)) != 0) // Ensure that there is only one flag in the value (i.e. value is a power of 2).
+            {
+                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Strings.ArgumentOutOfRange_InvalidEnumInSet, parameterName, value, enumFlagSet.ToString()), parameterName);
+            }
+            Contract.EndContractBlock();
+        }        
     }
 }
