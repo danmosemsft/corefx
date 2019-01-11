c976bb6c657e635a77523b4c0da5bfa43dcf1765
danmosemsft <danmose@microsoft.com>
2019-01-10 16:57:35 -0800

    More

--- src/System.Resources.Writer/src/System/Resources/MultitargetUtil.cs
+++ src/System.Resources.Writer/src/System/Resources/MultitargetUtil.cs
@@ -6,22 +6,54 @@ namespace System.Resources
 {
     internal static class MultitargetUtil
     {
-        internal static string GetAssemblyQualifiedName(Type type, Func<Type, string> func)
+        /// <devdoc>
+        ///     This method gets assembly info for the corresponding type. If the delegate
+        ///     is provided it is used to get this information.
+        /// </devdoc>
+        public static string GetAssemblyQualifiedName(Type type, Func<Type, string> typeNameConverter)
         {
-            return "FIXME";
+            string assemblyQualifiedName = null;
+
+            if (type != null)
+            {
+                if (typeNameConverter != null)
+                {
+                    try
+                    {
+                        assemblyQualifiedName = typeNameConverter(type);
+                    }
+                    catch (Exception e)
+                    {
+                        if (ClientUtils.IsSecurityOrCriticalException(e))
+                        {
+                            throw;
+                        }
+                    }
+                }
+
+                if (string.IsNullOrEmpty(assemblyQualifiedName))
+                {
+                    assemblyQualifiedName = type.AssemblyQualifiedName;
+                }
+            }
+
+            return assemblyQualifiedName;
         }
     }
 
     internal static class ClientUtils
     {
-        internal static bool IsCriticalException(Exception ex)
-        {
-            return false; // FIXME
+        public static bool IsCriticalException(Exception ex) {
+            return ex is NullReferenceException
+                    || ex is StackOverflowException
+                    || ex is OutOfMemoryException
+                    || ex is System.Threading.ThreadAbortException
+                    || ex is IndexOutOfRangeException
+                    || ex is AccessViolationException;
         }
 
-        internal static bool IsSecurityOrCriticalException(Exception ex)
-        {
-            return false; // FIXME
+        public static bool IsSecurityOrCriticalException(Exception ex) {
+            return (ex is System.Security.SecurityException) || IsCriticalException(ex);
         }
     }
 }
