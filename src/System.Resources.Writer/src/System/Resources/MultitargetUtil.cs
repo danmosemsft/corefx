a25d472f641d84e53af2902b8c18a87f7d159c4b
danmosemsft <danmose@microsoft.com>
2019-01-10 16:28:40 -0800

    Resx in S.R.W

---
+++ src/System.Resources.Writer/src/System/Resources/MultitargetUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Resources
+{
+    internal static class MultitargetUtil
+    {
+        internal static string GetAssemblyQualifiedName(Type type, Func<Type, string> func)
+        {
+            return "FIXME";
+        }
+    }
+
+    internal static class ClientUtils
+    {
+        internal static bool IsCriticalException(Exception ex)
+        {
+            return false; // FIXME
+        }
+
+        internal static bool IsSecurityOrCriticalException(Exception ex)
+        {
+            return false; // FIXME
+        }
+    }
+}
---
+++ src/System.Resources.Writer/src/System/Resources/ResXResourceSet.cs
@@ -0,0 +1,53 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Resources {
+
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    using System;
+    using System.Reflection;
+    using Microsoft.Win32;
+    using System.Drawing;
+    using System.IO;
+    using System.ComponentModel;
+    using System.Collections;
+    using System.Resources;
+    /// <summary>
+    ///     ResX resource set.
+    /// </summary>
+    public class ResXResourceSet : ResourceSet {
+        /// <summary>
+        ///     Creates a resource set for the specified file.
+        /// </summary>
+        [
+            SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")  // Shipped like this in Everett.
+        ]
+        public ResXResourceSet(string fileName) : base(new ResXResourceReader(fileName)) {
+            ReadResources();
+        }
+        /// <summary>
+        ///     Creates a resource set for the specified stream.
+        /// </summary>
+        [
+            SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")  // Shipped like this in Everett.
+        ]
+        public ResXResourceSet(Stream stream) : base(new ResXResourceReader(stream)) {
+            ReadResources();
+        }
+        /// <summary>
+        ///     Gets the default reader type associated with this set.
+        /// </summary>
+        public override Type GetDefaultReader() {
+            return typeof(ResXResourceReader);
+        }
+        /// <summary>
+        ///     Gets the default writer type associated with this set.
+        /// </summary>
+        public override Type GetDefaultWriter() {
+            return typeof(ResXResourceWriter);
+        }
+    }
+}
